7da0edc9ec8b493f35a8d66f2b964d561ba49fbb
Azmi SAHIN <azmisahin@outlook.com>
2024-04-06 08:43:14 +0300

    🚧: Family Tree

--- life/src/components/AppearanceFamilyTree.vue
+++ life/src/components/AppearanceFamilyTree.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    FamilyTree
-    <canvas ref="canvasTree"></canvas>
-    <div v-for="(item, index) in messages">
-      {{ item.parentId || 0 }} - {{ item.id }} 
-    </div>
+
+    <tr v-for="(item, index) in messages" :key="index" :class="index % 2 === 0 ? 'even' : 'odd'">
+      <td>{{ item.id }}</td>
+      <td>{{ item.parent_id || 0 }}</td>
+      <td>{{ item.lifetime_seconds }}</td>
+      <td>{{ item.elapsed_lifespan }}</td>
+      <td>{{ item.lifecycle }}</td>
+      <td>{{ item.number_of_copies }}</td>
+      <td>{{ item.generation }}</td>
+      <td>{{ item.match_count }}</td>
+      <td>{{ item.fitness }}</td>
+    </tr>
+
   </div>
 </template>
 
 <script>
 export default {
- 
   props: {
     dataList: {
       type: Array,
@@ -19,11 +26,9 @@ export default {
   },
   computed: {
     messages() {
-
       return this.dataList || [];
     },
   },
-
 };
 </script>
 
