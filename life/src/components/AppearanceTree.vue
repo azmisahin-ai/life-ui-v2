3e8499218833cdf213001e2fbb410a971767a34a
Azmi SAHIN <azmisahin@outlook.com>
2024-04-06 07:21:24 +0300

    🚧: AppearanceTree

--- life/src/components/AppearanceTree.vue
+++ life/src/components/AppearanceTree.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="tree">
-    <h3>AppearanceTree</h3>
-    <canvas ref="appearanceTreeCanvas"></canvas>
-    <div v-for="(item, index) in messages">
-      {{ item.codes }}
-    </div>
+
+
   </div>
 </template>
 
@@ -20,31 +17,6 @@ export default {
   computed: {
     messages() {
       return this.dataList || [];
-    },
-  },
-  mounted() {
-    this.drawCanvas();
-  },
-  methods: {
-    drawCanvas() {
-      const canvas = this.$refs.simulationTreeCanvas;
-      const ctx = canvas.getContext('2d');
-
-      // Clear canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-      this.messages.forEach((data, index) => {
-        ctx.fillStyle = 'green';
-        ctx.beginPath();
-        ctx.arc(50 + index * 50, 50, data.id * 5, 0, Math.PI * 2);
-        ctx.fill();
-      });
-      console.log("AppearanceTree", this.messages)
-    },
-  },
-  watch: {
-    data() {
-      this.drawCanvas();
     },
   },
 };
